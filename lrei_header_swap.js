fcd21f2a05424e71a4a2070ab665d09206eb1c32
Toby <tzitsman@gmail.com>
2023-01-20 16:23:43 -0500

    Create lrei_header_swap.js

---
+++ lrei_header_swap.js
@@ -0,0 +1,52 @@
+//@OnlyCurrentDoc
+function onOpen() {
+  SpreadsheetApp
+    .getUi()
+    .createAddonMenu()
+    .addItem("Clean CSV headers", "csvRemoveNumericPrefixesFromHeaders")
+    .addToUi();
+}
+function csvRemoveNumericPrefixesFromHeaders() {
+  const sheet = SpreadsheetApp.getActiveSheet();
+  Logger.log("Active sheet", sheet ? sheet.getName() : null);
+  if (sheet) {
+    let range = sheet.getRange(1, 1, 1, sheet.getLastColumn());
+    const values = range.getValues();
+    for (let row in values) {
+      for (let col in values[row]) {
+        const matches = /^(\d+[^A-Za-z0-9])(.*)$/.exec(values[row][col]);
+        if (matches) {
+          const cellRow = parseInt(row) + 1;
+          const cellCol = parseInt(col) + 1;
+          const cell = sheet.getRange(cellRow, cellCol);
+          cell.setValue(matches[2]);
+          Logger.log(`Removed "${matches[1]}" prefix from cell ${cellRow}:${cellCol}`);
+        }
+      }
+    }
+    //range of columns where header need to be changed
+    range = sheet.getRange(1,49,1,69)
+    range.setValues(containerArray)
+  }
+
+}
+
+//I'm not responsible for the nested for loops, may come back and clean that up
+
+//array of values new headers should be
+
+let containerArray = [
+  ['H1 p1Relationship', 'H1 p2Relationship', 'user_id1', 'H1 p1Title', 'H1 p1NameFirst', 'H1 p1NameFamiliar',
+  'H1 p1NameMiddle', 'H1 p1NameLast', 'H1 p1Suffix', 'H1 p1Email', 'H1 p1PhoneCell', 'H1 p1PhoneWork',
+  'H1 p1Employer', 'H1 p1Occupation', 'H1 p1JobTitle', 'H1 p2Title', 'H1 p2NameFirst', 'H1 p2NameFamiliar',
+  'H1 p2NameMiddle', 'H1 p2NameLast', 'H1 p2Suffix', 'H1 p2Email', 'H1 p2PhoneCell', 'H1 p2PhoneWork',
+  'H1 p2Employer', 'H1 p2Occupation', 'H1 p2JobTitle', 'H1 address1', 'H1 address2', 'H1 city', 'H1 state',
+  'H1 zip', 'H1 country', 'H1 phoneHome', 'secondaryHouseholdID', 'H2 p1Relationship', 'H2 p2Relationship',
+  'user_id2', 'H2 p1Title', 'H2 p1NameFirst', 'H2 p1NameFamiliar', 'H2 p1NameMiddle', 'H2 p1NameLast',
+  'H2 p1Suffix', 'H2 p1Email', 'H2 p1PhoneCell', 'H2 p1PhoneWork', 'H2 p1Employer', 'H2 p1Occupation',
+  'H2 p1JobTitle', 'H2 p2Title', 'H2 p2NameFirst', 'H2 p2NameFamiliar', 'H2 p2NameMiddle', 'H2 p2NameLast',
+  'H2 p2Suffix', 'H2 p2Email', 'H2 p2PhoneCell', 'H2 p2PhoneWork', 'H2 p2Employer', 'H2 p2Occupation',
+  'H2 p2JobTitle', 'H2 address1', 'H2 address2', 'H2 city', 'H2 state', 'H2 zip', 'H2 country', 'H2 phoneHome']
+]
+
+
