75eb899325865a8157ed308d221e88089b566ad3
Toby <tzitsman@gmail.com>
2023-02-23 18:52:46 -0500

    Update sumDays.js

--- sumDays.js
+++ sumDays.js
@@ -1,23 +1,38 @@
 //function add all the numbers including below and including current (think factorials but addition) in google scripts
 
-function myFunction() {
-  function onOpen() {
+
+function onOpen() {
     SpreadsheetApp
     .getUi()
     .createAddonMenu()
     .addItem("Sum Days", "sumDays")
     .addToUi();
+}
+function sumDays() {
+  const sheet = SpreadsheetApp.getActiveSheet()
+  let cell = sheet.getRange(3,4,1,1)
+  let cellVal = cell.getValue()
+  let total = (Math.pow(cellVal,2) + cellVal)/2
+  let range = sheet.getRange(3,2,1,2)
+  range.setValues([[total,total]])
+  
+  // reset day
+  let newVals = []
+
+  for (let i = 2; i < 4; i++) {
+    let toAdd = sheet.getRange(11,i,1,1)
+    let current = sheet.getRange(4,i,1,1)
+    let addVal = toAdd.getValue()
+    let currentVal = current.getValue()
+    let newVal = addVal + currentVal
+    newVals.push(newVal)
   }
-  function sumDays() {
-    const sheet = SpreadsheetApp.getActiveSheet()
-    // order is row#, column #, # of rows, # of columns
-    let cell = sheet.getRange(3,4,1,1)
-    let cellVal = cell.getValue()
-    // for exponents in google scripts use Math.pow(x,n)
-    let total = (Math.pow(cellVal,2) + cellVal)/2
-    let range = sheet.getRange(3,2,1,2)
-    // each outer array is a row, inner array is values of columns
-    // # of values and format must match range
-    range.setValues([[total,total]])
-  }
+
+  let currentValArray = [newVals]
+  console.log(currentValArray)
+
+  sheet.getRange(4,2,1,2).setValues(currentValArray)
+
+  let reset = sheet.getRange(11, 2, 1, 2)
+  reset.setValues([[0,0]])
 }
